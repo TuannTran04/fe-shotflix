be00a26ca04b52b88a56c2fb70b4c74afc06d140
TuannTran04 <skttfaker25@gmail.com>
2023-09-12 15:28:09 +0700

    test chunksize

--- pages/test.jsx
+++ pages/test.jsx
@@ -6,7 +6,6 @@ import Plyr from "plyr";
 import { useRef } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
-import shaka from "shaka-player";
 
 const Test = () => {
   const refVideo = useRef();
@@ -36,7 +35,7 @@ const Test = () => {
     renderSingleMovie();
   }, []);
 
-  const setupPlyr = () => {
+  const setupPlyr = async () => {
     // Chuyển đổi dữ liệu phụ đề thành định dạng Plyr
     if (Object.keys(movie).length > 0) {
       const plyrTracks = movie.subtitles?.map((subtitle, index) => ({
@@ -53,7 +52,6 @@ const Test = () => {
         type: `${video.typeVideo}; codecs="avc1.4D401E, mp4a.40.2"`,
         size: video.sizeVideo,
       }));
-      console.log("plyrSources", plyrSources);
 
       // Khởi tạo Ply
       let player;
@@ -88,86 +86,27 @@ const Test = () => {
       }
 
       // Thiết lập tracks cho Plyr
-      // player.source = {
-      //   type: "video",
-      //   title: "Example title",
-      //   sources: plyrSources,
-      //   poster:
-      //     "https://firebasestorage.googleapis.com/v0/b/movie-the-stone-d9f38.appspot.com/o/files%2Friengminhanh-426x240_2023-9-8_11%3A37%3A41%2Ftattay.jpg?alt=media&token=bf80ba35-fb61-40f6-9910-00402f79183e",
-      //   tracks: plyrTracks,
-      // };
+      player.source = {
+        type: "video",
+        title: "Example title",
+        sources: plyrSources,
+        poster:
+          "https://firebasestorage.googleapis.com/v0/b/movie-the-stone-d9f38.appspot.com/o/files%2Friengminhanh-426x240_2023-9-8_11%3A37%3A41%2Ftattay.jpg?alt=media&token=bf80ba35-fb61-40f6-9910-00402f79183e",
+        tracks: plyrTracks,
+      };
     }
   };
 
-  // useEffect(() => {
-  //   setupPlyr();
-  //   // Xóa sự kiện và Plyr instance khi unmount
-  //   return () => {
-  //     if (playerInstance) {
-  //       console.log("playerInstance", playerInstance);
-  //       playerInstance.destroy();
-  //       window.location.reload();
-  //     }
-  //   };
-  // }, [movie]);
-
-  function initApp() {
-    // Install built-in polyfills to patch browser incompatibilities.
-    shaka.polyfill.installAll();
-
-    // Check to see if the browser supports the basic APIs Shaka needs.
-    if (shaka.Player.isBrowserSupported()) {
-      // Everything looks good!
-      initPlayer();
-    } else {
-      // This browser does not have the minimum set of APIs we need.
-      console.error("Browser not supported!");
-    }
-  }
-
-  async function initPlayer() {
-    console.log("test shaka");
-    // Create a Player instance.
-    // const video = document.getElementById("video");
-    const player = new shaka.Player(refVideo.current);
-
-    // Attach player to the window to make it easy to access in the JS console.
-    window.player = player;
-
-    // Listen for error events.
-    player.addEventListener("error", onErrorEvent);
-
-    // Try to load a manifest.
-    // This is an asynchronous process.
-    try {
-      await player.load(
-        `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/video/nangamxadan_mp4.mp4?specificFolder=${movie.folderOnFirebase}`
-      );
-      // This runs if the asynchronous load is successful.
-      console.log("The video has now been loaded!");
-    } catch (e) {
-      // onError is executed if the asynchronous load fails.
-      onError(e);
-    }
-  }
-
-  function onErrorEvent(event) {
-    // Extract the shaka.util.Error object from the event.
-    onError(event.detail);
-  }
-
-  function onError(error) {
-    // Log the error.
-    console.error("Error code", error.code, "object", error);
-  }
-
-  // document.addEventListener("DOMContentLoaded", initApp);
-
   useEffect(() => {
-    if (movie) {
-      initApp();
-    }
+    setupPlyr();
     // Xóa sự kiện và Plyr instance khi unmount
+    return () => {
+      if (playerInstance) {
+        console.log("playerInstance", playerInstance);
+        playerInstance.destroy();
+        window.location.reload();
+      }
+    };
   }, [movie]);
 
   return (
