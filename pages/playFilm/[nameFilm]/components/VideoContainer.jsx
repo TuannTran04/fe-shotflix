a2afeb5a9c58e55ea78f3c575a0d6c19b6fa031c
TuannTran04 <skttfaker25@gmail.com>
2023-10-13 23:55:53 +0700

    fix comp videooo

--- pages/playFilm/[nameFilm]/components/VideoContainer.jsx
+++ pages/playFilm/[nameFilm]/components/VideoContainer.jsx
@@ -266,20 +266,174 @@ const VideoContainer = ({ movie, nameFilm }) => {
           // true
         ) {
           if (refVideo.current) {
-            alert("khong sp");
+            const plyrTracks = movie.subtitles?.map((subtitle, index) => ({
+              kind: "captions",
+              label: `${subtitle.langSubtitle} captions`,
+              srcLang: subtitle.langSubtitle,
+              src: `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/subtitles/${subtitle.subtitle}?specificFolder=${movie.folderOnFirebase}`,
+              default: index === 0, // Đánh dấu phụ đề đầu tiên là mặc định
+            }));
+            // console.log("plyrTracks", plyrTracks);
+
+            const plyrSources = movie.sources?.map((video, index) => ({
+              // src: `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/video/${video.srcVideo}?specificFolder=${movie.folderOnFirebase}`,
+              // src: `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/videoHLS/test_hls/v240p/index.m3u8`,
+
+              src: `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/videoHLS/${
+                movie.folderOnFirebase
+              }/${movie.video?.[0].trim()}`,
+              // src: `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/videoHLS/HoangDung/nangtho.mp4`,
+
+              // type: video.typeVideo,
+              // type: "application/x-mpegURL",
+              type: "application/x-mpegurl",
+
+              // size: video.sizeVideo,
+            }));
+            // alert("khong sp");
             // refVideo.current.src = `/neudanhmatem.mp4`;
             // refVideo.current.src = `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/videoHLS/JustaTee/bangkhuang.m3u8`;
 
-            refVideo.current.src = `${
-              process.env.NEXT_PUBLIC_URL
-            }/api/v1/movie/videoHLS/${
-              movie.folderOnFirebase
-            }/${movie.video?.[0].trim()}`;
+            // refVideo.current.src = `${
+            //   process.env.NEXT_PUBLIC_URL
+            // }/api/v1/movie/videoHLS/${
+            //   movie.folderOnFirebase
+            // }/${movie.video?.[0].trim()}`;
             // refVideo.current.src = `${process.env.NEXT_PUBLIC_URL}/api/v1/movie/videoHLS/chamvaomay_hongson/master.m3u8`;
-            refVideo.current.type = "application/x-mpegURL";
+            // refVideo.current.type = "application/x-mpegURL";
             // refVideo.current.addEventListener("loadedmetadata", function () {
             //   refVideo.current.play();
             // });
+
+            // Initialize PLYR here
+            player = new Plyr(refVideo.current, {
+              title: "Example Title",
+              controls: [
+                "play-large",
+                // "restart",
+                "rewind",
+                "play",
+                "fast-forward",
+                "progress",
+                "current-time",
+                "duration",
+                "mute",
+                "volume",
+                "captions",
+                "settings",
+                "pip",
+                "airplay",
+                "fullscreen",
+              ],
+              settings: ["captions", "quality", "speed", "loop"],
+              captions: { active: true, language: "vi", update: true },
+              tooltips: { controls: true, seek: true },
+              keyboard: { focused: true, global: true },
+              markers: {
+                enabled: true,
+                points: [{ time: 50, label: "con cec" }],
+              },
+              fullscreen: {
+                enabled: true,
+                fallback: true,
+                iosNative: true,
+                container: null,
+              },
+              disableContextMenu: false,
+              playsinline: true,
+              enabled: true,
+              ...defaultOptions,
+              // debug: true,
+            });
+
+            /////////////////////////////
+            // // Thiết lập tracks cho Plyr
+            player.source = {
+              type: "video",
+              title: "Example title",
+              sources: plyrSources,
+              tracks: plyrTracks,
+            };
+            /////////////////////////////
+
+            // Đặt sự kiện cho Plyr khi video load xong các data
+            if (player.playing == false) {
+              player.on("loadedmetadata", (event) => {
+                // console.log("readyyyy start");
+                // console.log(event.detail.plyr);
+                // console.log(event.detail.plyr.duration);
+                // Kiểm tra nếu có trạng thái xem video trong Local Storage
+                // const savedPlaybackTime = JSON.parse(
+                //   decryptData(localStorage.getItem(`${movie?._id}`), secretKey)
+                // );
+
+                const savedPlaybackTime = JSON.parse(
+                  localStorage.getItem(`${movie?._id}`)
+                );
+                // console.log(savedPlaybackTime);
+                const currTimeLocal = savedPlaybackTime?.currentTime;
+                const videoIdLocal = savedPlaybackTime?.videoId;
+
+                if (videoIdLocal == movie._id && event.detail.plyr.duration) {
+                  console.log("savedPlaybackTime canplay");
+
+                  const setPlayerCurrentTime = (currentTime) => {
+                    setTimeout(() => {
+                      const minutes = Math.floor(currentTime / 60);
+                      const seconds = Math.round(currentTime % 60);
+                      if (true) {
+                        // console.log(player.current);
+                        // console.log("continue");
+                        event.detail.plyr.muted = false;
+                        event.detail.plyr.currentTime = currentTime;
+                        // player.play();
+                      } else {
+                        console.log("begin");
+                        player.muted = true;
+                        player.currentTime = 0;
+                        player.play();
+                      }
+                    }, 500);
+                  };
+                  setPlayerCurrentTime(currTimeLocal);
+                }
+              });
+            }
+
+            // Đặt sự kiện cho Plyr khi video được update time
+            player.on("timeupdate", (event) => {
+              // console.log("Video is timeupdate");
+              let currentTime = event.detail.plyr.currentTime;
+              const duration = event.detail.plyr.duration;
+
+              // Thời gian hiện tại gần cuối video (1 giây trước khi kết thúc)
+              if (duration && duration - currentTime < 1) {
+                // console.log("Video đã xem xong");
+                // Thực hiện các tác vụ khi video kết thúc hoặc đã xem xong
+                localStorage.removeItem(`${movie?._id}`);
+              }
+
+              // currTime > 0 và khi video chưa kết thúc thì set localStorage time update
+              if (
+                currentTime &&
+                currentTime > 0 &&
+                duration &&
+                duration - currentTime > 1
+              ) {
+                // console.log("sett", {
+                //   currentTime: currentTime,
+                //   videoId: movie._id,
+                // });
+
+                localStorage.setItem(
+                  `${movie?._id}`,
+                  JSON.stringify({
+                    currentTime: currentTime,
+                    videoId: movie._id,
+                  })
+                );
+              }
+            });
           }
         }
       }
