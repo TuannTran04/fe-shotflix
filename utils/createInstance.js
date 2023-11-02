d76cb78be9527041c421f7ef93fbcfe809966653
TuannTran04 <skttfaker25@gmail.com>
2023-11-02 22:23:34 +0700

    add SEOo000

--- utils/createInstance.js
+++ utils/createInstance.js
@@ -1,102 +1,102 @@
-// import axios from "axios";
-// import jwt_decode from "jwt-decode";
-// import { useEffect } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
 
-// const refreshToken = async () => {
-//   try {
-//     console.log("call api refresh");
-//     // truyen len cookies
-//     const base_url = process.env.NEXT_PUBLIC_URL;
-//     const res = await axios.get(`${base_url}/api/v1/auth/refresh`, {
-//       withCredentials: true,
-//     });
-//     console.log(res);
+const refreshToken = async () => {
+  try {
+    console.log("call api refresh");
+    // truyen len cookies
+    const base_url = process.env.NEXT_PUBLIC_URL;
+    const res = await axios.get(`${base_url}/api/v1/auth/refresh`, {
+      withCredentials: true,
+    });
+    console.log(res);
 
-//     return res.data;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// export const createAxios = (user, dispatch, stateSuccess) => {
-//   console.log("user createAxios", user);
-//   const newInstance = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_URL,
-//     headers: { "Content-Type": "application/json" },
-//     withCredentials: true,
-//   });
+export const createAxios = (user, dispatch, stateSuccess) => {
+  console.log("user createAxios", user);
+  const newInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_URL,
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
 
-//   const requestIntercept = newInstance.interceptors.request.use(
-//     (config) => {
-//       console.log(">>> requestIntercept: <<<", config);
+  const requestIntercept = newInstance.interceptors.request.use(
+    (config) => {
+      console.log(">>> requestIntercept: <<<", config);
 
-//       // const decodedToken = jwt_decode(user?.accessToken);
-//       // console.log(">>> decodedToken : <<<", decodedToken);
+      // const decodedToken = jwt_decode(user?.accessToken);
+      // console.log(">>> decodedToken : <<<", decodedToken);
 
-//       if (!config.headers["Authorization"]) {
-//         console.log("khong co authorize thi gan'");
-//         config.headers["Authorization"] = `Bearer ${user?.accessToken}`;
+      if (!config.headers["Authorization"]) {
+        console.log("khong co authorize thi gan'");
+        config.headers["Authorization"] = `Bearer ${user?.accessToken}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  const responseIntercept = newInstance.interceptors.response.use(
+    (response) => {
+      console.log(">>> responseIntercept: <<<", response);
+      return response;
+    },
+    async (error) => {
+      const prevRequest = error?.config;
+      console.log(error);
+      if (error?.response?.status === 403 && !prevRequest?.sent) {
+        console.log("refresh tokennnnnnnnnnnnnnnnnnnnnnnn");
+        prevRequest.sent = true;
+        const newAccessToken = await refreshToken();
+        const refreshUser = {
+          ...user,
+          accessToken: newAccessToken.accessToken,
+        };
+        console.log(">>> refreshUser new <<<", refreshUser);
+        if (dispatch && stateSuccess) {
+          console.log("dispatch ok");
+          dispatch(stateSuccess(refreshUser));
+        }
+
+        prevRequest.headers[
+          "Authorization"
+        ] = `Bearer ${newAccessToken.accessToken}`;
+        return newInstance(prevRequest);
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return newInstance;
+};
+
+// newInstance.interceptors.request.use(
+//   async (config) => {
+//     let date = new Date();
+//     const decodedToken = jwt_decode(user?.accessToken);
+//     //   check accesstoken expires , get new token with refresh token
+//     if (decodedToken.exp < date.getTime() / 1000) {
+//       const data = await refreshToken();
+//       const refreshUser = {
+//         ...user,
+//         accessToken: data.accessToken,
+//       };
+//       if (dispatch && stateSuccess) {
+//         dispatch(stateSuccess(refreshUser));
 //       }
-//       return config;
-//     },
-//     (error) => Promise.reject(error)
-//   );
-
-//   const responseIntercept = newInstance.interceptors.response.use(
-//     (response) => {
-//       console.log(">>> responseIntercept: <<<", response);
-//       return response;
-//     },
-//     async (error) => {
-//       const prevRequest = error?.config;
-//       console.log(error);
-//       if (error?.response?.status === 403 && !prevRequest?.sent) {
-//         console.log("refresh tokennnnnnnnnnnnnnnnnnnnnnnn");
-//         prevRequest.sent = true;
-//         const newAccessToken = await refreshToken();
-//         const refreshUser = {
-//           ...user,
-//           accessToken: newAccessToken.accessToken,
-//         };
-//         console.log(">>> refreshUser new <<<", refreshUser);
-//         if (dispatch && stateSuccess) {
-//           console.log("dispatch ok");
-//           dispatch(stateSuccess(refreshUser));
-//         }
-
-//         prevRequest.headers[
-//           "Authorization"
-//         ] = `Bearer ${newAccessToken.accessToken}`;
-//         return newInstance(prevRequest);
-//       }
-//       return Promise.reject(error);
+//       config.headers["token"] = "Bearer " + data.accessToken;
 //     }
-//   );
-
-//   return newInstance;
-// };
-
-// // newInstance.interceptors.request.use(
-// //   async (config) => {
-// //     let date = new Date();
-// //     const decodedToken = jwt_decode(user?.accessToken);
-// //     //   check accesstoken expires , get new token with refresh token
-// //     if (decodedToken.exp < date.getTime() / 1000) {
-// //       const data = await refreshToken();
-// //       const refreshUser = {
-// //         ...user,
-// //         accessToken: data.accessToken,
-// //       };
-// //       if (dispatch && stateSuccess) {
-// //         dispatch(stateSuccess(refreshUser));
-// //       }
-// //       config.headers["token"] = "Bearer " + data.accessToken;
-// //     }
-// //     //
-// //     return config;
-// //   },
-// //   (err) => {
-// //     return Promise.reject(err);
-// //   }
-// // );
-// // return newInstance;
+//     //
+//     return config;
+//   },
+//   (err) => {
+//     return Promise.reject(err);
+//   }
+// );
+// return newInstance;
