275a1d1799615ae41b82307f5c08d32fc4b1ff30
TuannTran04 <skttfaker25@gmail.com>
2023-11-02 22:31:37 +0700

    fix deploy

--- pages/watch.jsx
+++ pages/watch.jsx
@@ -1,32 +1,32 @@
-// import React from "react";
-// import JWPlayer from "@jwplayer/jwplayer-react";
+import React from "react";
+import JWPlayer from "@jwplayer/jwplayer-react";
 
-// class PlayerContainer extends React.Component {
-//   render() {
-//     const playlist = [
-//       {
-//         file: "neudanhmatem.mp4",
-//         image: "unknowAvatar.webp",
-//         tracks: [
-//           {
-//             file: "test.vtt",
-//             label: "Vietsub",
-//             kind: "captions",
-//             default: true,
-//           },
-//         ],
-//       },
-//     ];
+class PlayerContainer extends React.Component {
+  render() {
+    const playlist = [
+      {
+        file: "neudanhmatem.mp4",
+        image: "unknowAvatar.webp",
+        tracks: [
+          {
+            file: "test.vtt",
+            label: "Vietsub",
+            kind: "captions",
+            default: true,
+          },
+        ],
+      },
+    ];
 
-//     return (
-//       <div className="players-container w-[400px] h-[400px]">
-//         <JWPlayer
-//           library="https://content.jwplatform.com/libraries/j9BLvpMc.js"
-//           //   playlist='https://cdn.jwplayer.com/v2/playlists/B8FTSH9D'
-//           playlist={playlist}
-//         />
-//       </div>
-//     );
-//   }
-// }
-// export default PlayerContainer;
+    return (
+      <div className="players-container w-[400px] h-[400px]">
+        <JWPlayer
+          library="https://content.jwplatform.com/libraries/j9BLvpMc.js"
+          //   playlist='https://cdn.jwplayer.com/v2/playlists/B8FTSH9D'
+          playlist={playlist}
+        />
+      </div>
+    );
+  }
+}
+export default PlayerContainer;
